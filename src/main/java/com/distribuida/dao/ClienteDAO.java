b4dc3446631f637843823ea0a90b0fa9edc2a0d4
JR <jrperez@tecnologicoismac.edu.ec>
2026-05-20 12:52:10 -0500

    mi codigo

--- src/main/java/com/distribuida/dao/ClienteDAO.java
+++ src/main/java/com/distribuida/dao/ClienteDAO.java
@@ -6,4 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface ClienteDAO extends JpaRepository<Cliente, Integer> {
 
     // comentarios para git hub
+
+    // comentarios para git hub desde casa...
+
 }
