638dd1f22d938df4ba11dff67bbd5c48b1916bf7
JR <jrperez@tecnologicoismac.edu.ec>
2026-05-20 12:51:12 -0500

    mi primer versionamiento

--- src/main/java/com/distribuida/dao/ClienteDAO.java
+++ src/main/java/com/distribuida/dao/ClienteDAO.java
@@ -4,4 +4,6 @@ import com.distribuida.model.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ClienteDAO extends JpaRepository<Cliente, Integer> {
+
+    // comentarios para git hub
 }
